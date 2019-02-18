c4f33da6b34feb802970f1a7fc519a9674c4f1fb
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 01:23:05 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan4-Kambing/src/pbo2/pkg10116058/latihan4/kambing/PBO210116058Latihan4Kambing.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116058.latihan4.kambing;
+
+public class PBO210116058Latihan4Kambing {
+    public void tambahKambing(){
+    //Deklarasi Variabel lokal
+    int jumlahKambing=0;
+    
+    jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah Ditamah : " +
+                jumlahKambing);
+    
+    }   
+    public static void main(String[] args) {
+       PBO210116058Latihan4Kambing kambingJantan = new PBO210116058Latihan4Kambing();
+       kambingJantan.tambahKambing();
+        
+    }
+}
